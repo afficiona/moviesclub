c2a3a69f2d84805bf00b01d07f4145e65389ccd4
Aman Pandey <aman.pandey@rksv.in>
2019-09-01 14:35:06 +0530

    first commit

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -2,13 +2,11 @@ const APP_CONSTANTS = {};
 
 const API_CONSTANTS = {
   FALLBACK_ERROR_MESSAGE: 'Something went wrong. Please try again later!',
-  API_BASE_URL: 'http://localhost:3001',
+  API_BASE_URL: 'https://afficiona-movies.herokuapp.com',
 };
 const ENV_CONSTANTS = {
   development: {},
-  production: {
-    API_BASE_URL: 'https://afficiona-movies.herokuapp.com',
-  },
+  production: {},
 };
 
 export default Object.assign(APP_CONSTANTS, API_CONSTANTS, ENV_CONSTANTS[process.env.REACT_APP_ENV]);
