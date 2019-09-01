c9dd4407d94fe607ce43c7e8c971690bc2d0b207
Aman Pandey <aman.pandey@rksv.in>
2019-09-01 14:26:32 +0530

    Updates

---
+++ precache-manifest.7686651ca835f8d7fe0f0c321c88e22e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "43d3d89928bf94d55452cfc7ce58676c",
+    "url": "/movies-list/index.html"
+  },
+  {
+    "revision": "2e596ea985ed42eb909c",
+    "url": "/movies-list/static/css/main.42968cae.chunk.css"
+  },
+  {
+    "revision": "e0cc2b2c44644683183a",
+    "url": "/movies-list/static/js/2.fe7ffc8f.chunk.js"
+  },
+  {
+    "revision": "2e596ea985ed42eb909c",
+    "url": "/movies-list/static/js/main.01c8ebe0.chunk.js"
+  },
+  {
+    "revision": "0a40861e2904023f5cbc",
+    "url": "/movies-list/static/js/runtime~main.94ac7d43.js"
+  }
+]);
